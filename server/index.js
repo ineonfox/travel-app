686ca8a4938c3d4901aa663b4518c445bcc8b899
ineonfox <mykhailo.masliukh@gmail.com>
2023-05-11 09:06:40 +0200

    Initialize the project

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+  
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+/*const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'dbuser',
+  password: 's3kreee7',
+  database: 'my_db'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()*/
